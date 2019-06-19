40f825e17f3a8bb3a76878182bc90fa57b7e363d
BillyPittman1993 <billy.pittman@flatironschool.com>
2019-06-19 18:04:44 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,7 @@ require "pry"
 
 def speak_to_grandma(phrase)
   if phrase == 'Hi Nana, how are you?'
-    return "HUH?! SPEAK UP, SONNY!"2.times
+    return "HUH?! SPEAK UP, SONNY!".times.2
   elsif phrase.upcase == true 
     return "NO, NOT SINCE 1938!"
   elsif phrase == "I love you grandma!".upcase
