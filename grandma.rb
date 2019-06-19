161132054201595e4e4d30ddccbf7e6a1da25dd1
BillyPittman1993 <billy.pittman@flatironschool.com>
2019-06-19 17:50:53 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,11 +1,11 @@
 phrase = ""
 
 def speak_to_grandma(phrase)
-  if phrase.upcase? == false || nil 
+  if phrase.upcase == false || nil 
     return "HUH? SPEAK UP, SONNY!"
-  elsif phrase.upcase? == true 
+  elsif phrase.upcase == true 
     return "NO, NOT SINCE 1938!"
-  elsif phrase.upcase? == "I LOVE YOU GRANDMA!"
+  elsif phrase.upcase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN"
   end
 end
