f96d4f466ed8ad5df20666a7afc4aab2e4b49ab6
BillyPittman1993 <billy.pittman@flatironschool.com>
2019-06-19 17:47:20 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,9 +1,9 @@
 phrase = ""
 
 def speak_to_grandma(phrase)
-  if phrase.upcase?
-    return "NO, NOT SINCE 1938!"
-  elsif phrase == "I LOVE YOU GRANDMA!"
+  if phrase.upcase? == false || nil 
+    return "HUH? SPEAK UP, SONNY!"
+  elsif phrase.upcase? == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN"
   else
     return "HUH? SPEAK UP, SONNY!"
